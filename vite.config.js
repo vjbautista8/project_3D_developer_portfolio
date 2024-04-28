b0afba82a69632d6b5578989feab769d904deed2
vjbautista8 <vjbautista8@gmail.com>
2024-04-29 05:25:35 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/repo/',
+  base: '/project_3D_developer_portfolio/',
   plugins: [react()],
   server: {
     host: true,
